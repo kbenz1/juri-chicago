44fb517cc235588b850a9a7458b892991c951df5
Konstantin Benz <konstantin.benz@gmail.com>
2025-04-12 10:01:46 +0200

    kapitel 4 excel sheets

--- kapitel-4/main.py
+++ kapitel-4/main.py
@@ -9,23 +9,43 @@ output_file = "ergebnisse.xlsx"
 df = pd.read_excel(input_file)
 
 # Kreuztabelle erstellen
-cross_tab = pd.crosstab(df['Upround?'], df['Schwipps?'])
+cross_tab = pd.crosstab(df['Gruppe'], df['Diagnose'])
+
+# Entferne die Wörter "Gruppe" und "Diagnose"
+cross_tab.index.name = None
+cross_tab.columns.name = None
+
+print(cross_tab)
 
 # Chi-Quadrat-Test durchführen
-chi2, p, dof, expected = chi2_contingency(cross_tab)
+chi2, p, fre_grad, expected = chi2_contingency(cross_tab)
+
+# Erwartete Ergebnisse
+expected_df = pd.DataFrame(expected, index=cross_tab.index, columns=cross_tab.columns)
+
+# Zahlen auf null Kommastellen runden.
+expected_df = expected_df.round(0)
+
+print(expected_df)
 
 # Ergebnisse formatieren
 chi_square_results = {
     "Chi-Quadrat-Wert": [chi2],
-    "p-Wert": [p],
-    "Freiheitsgrade": [dof]
+    "Erreichtes Signifikanzniveau": [p if p >= 0.05 else "< 5%"],
+    "Freiheitsgrade": [fre_grad],
+    "Zusammenhang gefunden?": ["Nein" if p >= 0.05 else "Ja"]
 }
-expected_df = pd.DataFrame(expected, index=cross_tab.index, columns=cross_tab.columns)
+results = pd.DataFrame(chi_square_results)
+
+# Zahlen auf 4 Kommastellen runden.
+results = results.round(4)
+
+print(results)
 
 # Ergebnisse in ein Excel schreiben
 with pd.ExcelWriter(output_file, engine="openpyxl") as writer:
     cross_tab.to_excel(writer, sheet_name="Kreuztabelle")
-    pd.DataFrame(chi_square_results).to_excel(writer, sheet_name="Chi-Quadrat-Ergebnisse")
     expected_df.to_excel(writer, sheet_name="Erwartete Werte")
+    results.to_excel(writer, sheet_name="Chi-Quadrat-Ergebnisse", index=False)
 
 print(f"Die Ergebnisse wurden erfolgreich in '{output_file}' gespeichert.")
